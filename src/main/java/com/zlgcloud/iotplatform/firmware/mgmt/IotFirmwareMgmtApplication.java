bbe478ffb88de01e21af70b83988f33eb989541c
lidaheng <linany1990@163.com>
2021-09-01 11:00:53 +0800

    add api

---
+++ src/main/java/com/zlgcloud/iotplatform/firmware/mgmt/IotFirmwareMgmtApplication.java
@@ -0,0 +1,11 @@
+package com.zlgcloud.iotplatform.firmware.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotFirmwareMgmtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IotFirmwareMgmtApplication.class,args);
+    }
+}
